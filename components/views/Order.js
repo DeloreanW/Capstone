806efef4aa73d7576bfdd9f0237b7b57f652a687
delorean williams <deloreanwilliams25@gmail.com>
2024-04-04 18:11:32 -0500

    Updated capstone

--- components/views/Order.js
+++ components/views/Order.js
@@ -8,6 +8,53 @@ export default state => html`
           return `<option value="${shoe.name}">${shoe.name}</option>`;
         })}</select
       >
+      <label for="Size">Size:</label>
+        <select id="Size" name="Size">
+          <option value="">Select a Size</option>
+          <option value="7">7</option>
+          <option value="7.5">7.5</option>
+          <option value="8">8</option>
+          <option value="8.5">8.5</option>
+          <option value="9">9</option>
+          <option value="9.5">9.5</option>
+          <option value="10">10</option>
+          <option value="10.5">10.5</option>
+          <option value="11">11</option>
+          <option value="11.5">11.5</option>
+          <option value="12">12</option>
+          <option value="12.5">12.5</option>
+          <option value="13">13</option>
+          <option value="13.5">13.5</option>
+          <option value="14">14</option>
+          <option value="14.5">14.5</option>
+          <option value="15">15</option>
+        </select>
+      </div>
+      <div>
+        <label for="First Name">First Name:</label>
+        <input
+          type="text"
+          name="First Name"
+          id="First Name"
+          placeholder="First Name"
+          required
+        />
+      </div>
+      <div>
+        <label for="Last Name">Last Name:</label>
+        <input
+          type="text"
+          name="Last Name"
+          id="Last Name"
+          placeholder="Last Name"
+          required
+        />
+      </div>
+      <div>
+        <label for="Email">Email</label>
+        <input type="text" name="Email" id="Email" placeholder="Email" />
+      </div>
+      <input type="submit" name="submit" value="Subscribe" />
     </form>
   </section>
   ${state.shoes.map(shoe => console.log(shoe))}
--- components/views/Shoe.js
+++ components/views/Shoe.js
@@ -1,25 +1,13 @@
 import html from "html-literal";
 
 export default state => html`
-  <section id="Brand">
-    <table id="Region">
-      <div class="dropdown">
-        <button onclick="myFunction()" class="dropbtn">Dropdown</button>
-        <div id="myDropdown" class="dropdown-content">
-          <a href="United States">Link 1</a>
-          <a href="Europe">Link 2</a>
-        </div>
-      </div>
       <tr>
         <section id="shoes">
           <div class="filter">
-            <select name="column" id="column">
-              <option value="">select a column</option>
-              <option value=""></option>
-              <option value=""></option>
-              <option value=""></option>
-              <option value=""></option>
-              <option value="customer">Customer</option>
+            <select name="region" id="region">
+              <option value="">select a region</option>
+              <option value="United States">United States</option>
+              <option value="Europe">Europe</option>
             </select>
             <input type="search" name="filter" id="filter" />
             <button id="search-button">Search</button>
@@ -30,14 +18,13 @@ export default state => html`
               <th></th>
               <th></th>
               <th></th>
-              <th>Customer</th>
+              <th></th>
             </tr>
             <tr>
               <th>United States</th>
               <th>Europe</th>
             </tr>
           </table>
-
           <ul>
             ${state.shoes.map(shoe => {
               return html`
